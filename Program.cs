033b20699ef96881872d5bacd9d5f5f2b435585c
Mateusz Łukaszewicz <mateusz.lukaszewicz@microsoft.wsei.edu.pl>
2022-11-07 15:46:03 +0100

    get set

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace nauka1
+{
+    public class c13
+    {
+        private float liczba;
+        public int dane { get; set; }
+
+    }
+    public class demo
+    {
+        public static void Main()
+        {
+            c13 obiekt = new c13();
+            obiekt.dane = 100;
+            Console.WriteLine(obiekt.dane);
+            Console.In.ReadLine();
+
+        }
+    }
+    
+    
+}
+
